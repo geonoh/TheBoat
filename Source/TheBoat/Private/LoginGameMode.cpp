1c3f0d412cbaccd0c7fd0d913b2ec07d82810fb7
Geon Oh <ohhh123@naver.com>
2024-05-19 16:50:41 +0900

    Login 레벨 추가
    - LoginGameMode 추가 (코드도 추가)
    
    - 카피랄이트 작성

---
+++ Source/TheBoat/Private/LoginGameMode.cpp
@@ -0,0 +1,5 @@
+// It's made by Geon Oh
+
+
+#include "LoginGameMode.h"
+
---
+++ Source/TheBoat/Public/LoginGameMode.h
@@ -0,0 +1,17 @@
+// It's made by Geon Oh
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LoginGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THEBOAT_API ALoginGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
