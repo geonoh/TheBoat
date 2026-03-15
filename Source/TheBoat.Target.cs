0232c2a590199fccd15d0e44422243a1d7f33d3f
Geon Oh <ohhh123@naver.com>
2026-03-15 14:16:58 +0900

    UE5 버전 업그레이드 및 서버 플랫폼 버전 업그레이드 + 서버 sln 파일은 git에서 tracking
    - Unreal Engine 5.7.4
    
    - Server v145

--- Source/TheBoat.Target.cs
+++ Source/TheBoat.Target.cs
@@ -8,8 +8,8 @@ public class TheBoatTarget : TargetRules
 	public TheBoatTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
 		ExtraModuleNames.Add("TheBoat");
 	}
 }
--- Source/TheBoatEditor.Target.cs
+++ Source/TheBoatEditor.Target.cs
@@ -8,8 +8,8 @@ public class TheBoatEditorTarget : TargetRules
 	public TheBoatEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
 		ExtraModuleNames.Add("TheBoat");
 	}
 }
